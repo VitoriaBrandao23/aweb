1fbef5661b5b888cb46cbf1f5c57b93bc14c1dbd
Vitória Beatriz Batista Brandão <132864073+VitoriaBrandao23@users.noreply.github.com>
2025-02-03 10:17:47 -0300

    Novas configurações
    falta ajeitar curiosidade perfeitamente, hero ok

--- bimestre2/paginaPessoal/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(() => {
-        document.body.style.overflow = "auto"; // Libera a rolagem
-        document.querySelector(".main-content").style.visibility = "visible"; // Exibe o restante do site
-    }, 3000); // Tempo de espera antes de exibir o restante do site (3 segundos)
-});
